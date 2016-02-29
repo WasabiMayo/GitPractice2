62ac31c0695183cac4fbf192fe564b651e104651
incepted <mintception@gmail.com>
2016-02-29 12:48:08 -0500

    refresh

--- GitPractice2/app/src/main/java/com/example/wasabi/gitpractice2/MainActivity.java
+++ GitPractice2/app/src/main/java/com/example/wasabi/gitpractice2/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.wasabi.gitpractice2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,5 +16,9 @@ public class MainActivity extends AppCompatActivity {
         Actor leo = new Actor("Leonardo DiCaprio", "Nov 11, 1974", "1");
         Actor matt = new Actor("Matt Damon", "Oct 8, 1970", "1");
         Actor jennifer = new Actor("Jennifer Lawrence", "Aug 15, 1990", "1");
+
+        ArrayList<Actor> mActorList = new ArrayList<>();
+        ActorAdapter adapter = new ActorAdapter(MainActivity.this, mActorList);
+        ListView list
     }
 }
