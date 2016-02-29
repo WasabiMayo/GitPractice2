9d3437008794464ac8345d83f2e4dffaebc5f5d7
WasabiMayo <hyewon910119@gmail.com>
2016-02-29 12:47:18 -0500

    Refresh

--- GitPractice2/app/src/main/java/com/example/wasabi/gitpractice2/MainActivity.java
+++ GitPractice2/app/src/main/java/com/example/wasabi/gitpractice2/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.wasabi.gitpractice2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,5 +17,15 @@ public class MainActivity extends AppCompatActivity {
         Actor leo = new Actor("Leonardo DiCaprio", "Nov 11, 1974", "1");
         Actor matt = new Actor("Matt Damon", "Oct 8, 1970", "1");
         Actor jennifer = new Actor("Jennifer Lawrence", "Aug 15, 1990", "1");
+
+        ArrayList<Actor> actorsList = new ArrayList<>();
+        actorsList.add(leo);
+        actorsList.add(matt);
+        actorsList.add(jennifer);
+
+        ActorAdapter adapter = new ActorAdapter(MainActivity.this, actorsList);
+
+        ListView listView = (ListView)findViewById(R.id.LV);
+        listView.setAdapter(adapter);
     }
 }
